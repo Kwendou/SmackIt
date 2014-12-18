bfcf44a7c90d806676365694b0dadf314ebfc913
Lagna <antoine.heaj@gmail.com>
2014-12-19 00:30:46 +0100

    Fini de préparer ... V0.1
    Demain on code dur … Enfin le vrai Challenge commence :) … Tous a vos
    clavier et à vos bouquin

---
+++ _Site/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="_style/reset.css">
+        <link rel="stylesheet" href="_style/style.css">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+    </head>
+    <body>
+        
+        <img class="template" src="_img/templat.png" alt="template" />
+        
+       <div class="container">
+           
+            <?php include("_pages/menu.php"); ?>
+            
+            <!-- ----- CONTENU ------ -->
+            <!-- ----- CONTENU ------ -->
+            <!-- ----- CONTENU ------ -->
+           
+            <?php include("_pages/home.php"); ?>
+            
+            <?php include("_pages/challenge.php"); ?>
+            
+            <?php include("_pages/success.php"); ?>
+            
+            <?php include("_pages/faillure.php"); ?>
+            
+            <?php include("_pages/profil.php"); ?>
+            
+            <?php include("_pages/player.php"); ?>
+            
+            <?php include("_pages/players_mondial.php"); ?>
+            
+            <!-- ----- FIN CONTENU ------ -->
+            <!-- ----- FIN CONTENU ------ -->
+            <!-- ----- FIN CONTENU ------ -->
+            
+            
+            <!-- ----- POP-UP ------ -->
+            <!-- ----- POP-UP ------ -->
+            <!-- ----- POP-UP ------ -->
+            
+            <?php include("_popUp/signUp.php"); ?>
+            
+            <?php include("_popUp/logIn_signUp.php"); ?>
+            
+            <?php include ("_popUp/challenge.php"); ?>
+            
+            <!-- ----- FIN POP-UP ------ -->
+            <!-- ----- FIN POP-UP ------ -->
+            <!-- ----- FIN POP-UP ------ -->
+            
+            
+           
+       </div>
+       
+       <script src="_js/Jquery.js"></script>
+       <script src="_js/main.js"></script>
+    
+    </body>
+</html>
