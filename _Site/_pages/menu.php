20bb5e3cea8b6a073c50d30da4a076e619237be5
Lagna <antoine.heaj@gmail.com>
2014-12-21 20:13:41 +0100

    plop

--- _Site/_pages/menu.php
+++ _Site/_pages/menu.php
@@ -1,39 +1,12 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title></title>
-        <link rel="stylesheet" href="_style/reset.css">
-        <link rel="stylesheet" href="_style/style.css">
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width">
-    </head>
-    <body>
-        
-       <div class="container">
-           
-            <!-- ----- menu ------ -->
-            <!-- ----- menu ------ -->
-            <!-- ----- menu ------ -->
-            
-            <nav class="menu">
-                
-                <ul class="nav">
-                    
-                    <li class="nav__el nav__el--active"><a href="#home">home</a></li>
-                    <li class="nav__el"><a href="#challenge">challenge</a></li>
-                    <li class="nav__el"><a href="#players">players</a></li>
-                    <li class="nav__el"><a href="#profil">profil</a></li>
-                    
-                </ul>
-                
-            </nav>
-            
-            <!-- ----- FIN menu ------ -->
-            <!-- ----- FIN menu ------ -->
-            <!-- ----- FIN menu ------ -->
-            
-       </div>
-    
-    </body>
-</html>
+<nav class="menu">
+
+    <ul class="nav">
+
+        <li class="nav__el nav__el--active"><a href="#home">home</a></li>
+        <li class="nav__el"><a href="#challenge">challenge</a></li>
+        <li class="nav__el"><a href="#players">players</a></li>
+        <li class="nav__el"><a href="#profil">profil</a></li>
+
+    </ul>
+
+</nav>
