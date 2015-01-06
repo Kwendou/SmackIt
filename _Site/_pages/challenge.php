5a244a913730b8ab2efa49a45ba7a44da5a5e8c0
Lagna <antoine.heaj@gmail.com>
2015-01-06 15:41:48 +0100

    challenge . php

---
+++ _Site/_pages/challenge.php
@@ -0,0 +1,27 @@
+<div class="content">
+              
+   <div class="easy">
+
+       <h2 class="selection">
+           Facile
+       </h2>
+
+   </div>
+
+   <div class="hard">
+
+       <h2 class="selection">
+           Difficile
+       </h2> 
+
+   </div>
+
+   <div class="yolo">
+
+       <h2 class="selection">
+          Yolo 
+       </h2>
+
+   </div>
+
+</div>
